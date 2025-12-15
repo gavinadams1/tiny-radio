f1f9116a7f8690ce0d1f807796c77e92b6dc1348
Gavin <gavinadamsdev@gmail.com>
2025-12-15 12:54:05 +0000

    simplifying backup script

--- start-backup.sh
+++ start-backup.sh
@@ -1,28 +1,16 @@
 #!/usr/bin/env bash
 
-STREAM_KEY="YOUR_YOUTUBE_STREAM_KEY"
-RTMP_URL="rtmp://b.rtmp.youtube.com/live2/6s2p-ku56-wqf1-7k40-d4ja?backup=1&"
-VIDEO="./loop.mp4"
-MESSAGE="./message.txt"
-FONT="/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf"
-
 ffmpeg \
   -hide_banner -loglevel info \
-  -stream_loop -1 -re -i "$VIDEO" \
+  -stream_loop -1 -re -i loop.mp4 \
   -f lavfi -i anullsrc=channel_layout=stereo:sample_rate=44100 \
-  -filter_complex "\
-[0:v]scale=640:360,fps=15,drawtext=fontfile=${FONT}:textfile=${MESSAGE}:reload=1:fontcolor=white:fontsize=48:x=(w-text_w)/2:y=(h-text_h)/2:box=1:boxcolor=black@0.6:boxborderw=20[v]" \
+  -filter_complex "[0:v]scale=640:360,fps=8,drawtext=fontfile=/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf:text='Be back soon':fontcolor=white:fontsize=28:x=(w-text_w)/2:y=(h-text_h)/2:box=1:boxcolor=black@0.6:boxborderw=14[v]" \
   -map "[v]" -map 1:a \
-  -c:v libx264 \
-  -preset ultrafast \
-  -tune stillimage \
-  -pix_fmt yuv420p \
-  -g 60 -keyint_min 60 \
-  -b:v 1500k \
-  -maxrate 1500k \
-  -bufsize 3000k \
-  -c:a aac \
-  -b:a 128k \
-  -ar 44100 \
   -r 8 \
-  -f flv "$RTMP_URL"
+  -c:v libx264 -preset ultrafast -tune stillimage \
+  -b:v 250k -maxrate 250k -bufsize 500k \
+  -pix_fmt yuv420p \
+  -g 32 -keyint_min 32 \
+  -c:a aac -b:a 96k -ar 44100 \
+  -f flv "rtmp://b.rtmp.youtube.com/live2/6s2p-ku56-wqf1-7k40-d4ja?backup=1&"
+
