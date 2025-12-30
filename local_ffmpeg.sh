eaea4c11e5c7a82b28342ee9f65ce839e1abeed0
Gavin <gavinadamsdev@gmail.com>
2025-12-30 20:47:51 +0000

    updating loops

--- local_ffmpeg.sh
+++ local_ffmpeg.sh
@@ -1,7 +1,7 @@
-ffmpeg -loop 1 -i background.jpg \
+ffmpeg -loop 1 -i night_background.jpg \
   -vf "scale=640:360,fps=8" \
   -c:v libx264 -preset slow -tune stillimage \
   -b:v 2500k -maxrate 2500k -bufsize 5000k \
   -pix_fmt yuv420p \
   -t 300 \
-  loop.mp4
+  loop_night.mp4
